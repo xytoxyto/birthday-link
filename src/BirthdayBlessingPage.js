fc595806e3718557c7c1eb5233fc3daf33fc147a
xytoxyto <trooks8@gmail.com>
2025-07-15 18:42:30 -0400

    feat: add BirthdayBlessingPage component and include OpenAI dependency

---
+++ src/BirthdayBlessingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// Import Link when you need to use it
+// import Link from 'next/link'; // or import { Link } from 'react-router-dom';
+
+function BirthdayBlessingPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-blue-900 to-black text-white py-6 px-4 sm:px-6 lg:px-8">
+      {/* ... the JSX code you provided ... */}
+    </div>
+  );
+}
+
+export default BirthdayBlessingPage;
