da8a14b850299d6aae7bcf85ba058c0576183cee
xytoxyto <trooks8@gmail.com>
2025-07-15 17:17:57 -0400

    feat: update HomePage to include title and login link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,11 @@
+import Link from 'next/link';
 import SignupForm from '@/components/SignupForm';
 
 export default function HomePage() {
   return (
     <main>
+      <h1>Home Page</h1>
+      <Link href="/login">Login</Link>
       <SignupForm />
     </main>
   );
