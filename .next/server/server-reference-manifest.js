f4e5d9d1d9563135c0fc94ba9a4c4ff0a30d7ec3
xytoxyto <trooks8@gmail.com>
2025-07-15 21:28:28 -0400

    feat: add new pages and layout for birthday blessing and invite features
    - Implemented new page structure for birthday blessing and invite functionalities.
    - Added type checks for page properties and metadata generation.
    - Introduced utility types for better type safety in page props.
    - Updated global CSS variables for consistent theming across the application.
    - Created build task for Next.js app in VSCode configuration.

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"re9i/fNpoFeT128DvwKZorY/7rekCWDQna02pffRtBE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,4 +1,6 @@
 
+
+
 import Link from 'next/link';
 import { type Metadata } from 'next';
 
